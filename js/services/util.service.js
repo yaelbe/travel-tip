593395bf1a4117d773ec5c49dc4bb03fe52b7a3a
Yael B <yael@any.do>
2023-02-22 16:21:56 +0200

    add query string params to utils

--- js/services/util.service.js
+++ js/services/util.service.js
@@ -1,53 +1,80 @@
 export const utilService = {
-    saveToStorage,
-    loadFromStorage,
-    makeId,
-    randomPastTime,
-    randomPetName,
-    randomPetType,
-}
-
-const gPetNames = ['Bob', 'Charls', 'Chip']
-const gPetTypes = ['cat', 'dog', 'bird', 'fish', 'rabbit']
-
-function saveToStorage(key, value) {
-    localStorage.setItem(key, JSON.stringify(value))
-}
-
-function loadFromStorage(key) {
-    const data = localStorage.getItem(key)
-    return (data) ? JSON.parse(data) : undefined
+  getRandomInt,
+  makeId,
+  randomPastTime,
+  setQueryParams,
+  deleteQueryParam,
+  getValFromParam,
 }
 
 function makeId(length = 5) {
-    var txt = ''
-    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-    for (var i = 0; i < length; i++) {
-        txt += possible.charAt(Math.floor(Math.random() * possible.length))
-    }
-    return txt
+  var txt = ''
+  var possible = 'abcdefghijklmnopqrstuvwxyz0123456789'
+  for (var i = 0; i < length; i++) {
+    txt += possible.charAt(Math.floor(Math.random() * possible.length))
+  }
+  return txt
 }
-
 
 function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min)
-    max = Math.floor(max)
-    return Math.floor(Math.random() * (max - min + 1) + min)
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
-function randomPetName() {
-    return gPetNames[parseInt(Math.random() * gPetNames.length)]
-}
-
-function randomPetType() {
-    return gPetTypes[parseInt(Math.random() * gPetTypes.length)]
+function getRandomInt(min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min)) + min
 }
 
 function randomPastTime() {
-    const HOUR = 1000 * 60 * 60
-    const DAY = 1000 * 60 * 60 * 24
-    const WEEK = 1000 * 60 * 60 * 24 * 7
-    
-    const pastTime = getRandomIntInclusive(HOUR, WEEK)
-    return Date.now() - pastTime
+  const HOUR = 1000 * 60 * 60
+  const DAY = 1000 * 60 * 60 * 24
+  const WEEK = 1000 * 60 * 60 * 24 * 7
+
+  const pastTime = getRandomIntInclusive(HOUR, WEEK)
+  return Date.now() - pastTime
+}
+
+function setQueryParams(newParams) {
+  const url = new URL(window.location.href)
+  const params = new URLSearchParams(url.search)
+
+  for (var paramName in newParams) {
+    const paramValue = newParams[paramName]
+    params.set(paramName, paramValue) // used to update an existing query string parameter or add a new one if it doesn't exist.
+  }
+
+  url.search = params.toString()
+  window.history.pushState({ path: url.href }, '', url.href) //modify the URL of the current page without reloading the page
+}
+
+function deleteQueryParam(key) {
+  const url = new URL(window.location.href)
+  const params = new URLSearchParams(url.search)
+
+  params.delete(key)
+  url.search = params.toString()
+
+  window.history.pushState({ path: url.href }, '', url.href)
+}
+
+function getValFromParam(key) {
+  const queryStringParams = new URLSearchParams(window.location.search)
+  return queryStringParams.get(key)
+}
+
+function formatTime(date) {
+  const options = {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    hour12: false,
+  }
+
+  return new Intl.DateTimeFormat('default', options).format(date)
 }
